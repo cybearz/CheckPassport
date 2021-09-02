46291d0271c01b98e3e7cc685743150d93a06e29
Nemcron <goryashin.sanya@gmail.com>
2021-09-02 17:05:03 +0300

    Created user's profile button in appbar

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,13 @@
 				<v-icon size="40">mdi-passport</v-icon>
 				<div class="text-h4">CheckPassport</div>
 			</div>
+
+			<v-spacer></v-spacer>
+
+			<v-btn>
+				<v-icon left>mdi-account</v-icon>
+				Личный кабинет
+			</v-btn>
 		</v-app-bar>
 
 		<v-main>
