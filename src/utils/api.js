7dc4d36bf47f94bebdca7285a1d33e9057b0dfc8
Alex <alex@yavent.ru>
2021-09-16 15:18:45 +0300

    refactor(api): LocalStorageData & api get/set functions added

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,26 +1,65 @@
+//
+
+class LocalStorageData {
+
+	constructor(key) {
+		this.key = key
+	}
+
+	get() {
+		const v = localStorage.getItem(this.key)
+		return v ? JSON.parse(v) : null
+	}
+
+	set(v) {
+		localStorage.setItem(this.key, JSON.stringify(v))
+		return v
+	}
+}
+
+const empStore = new LocalStorageData("empStore")
+const empProfile = new LocalStorageData("empProfile")
+
+// api
+
+export function getEmpStore() {
+	return empStore.get()
+}
+
+export function setEmpStore(v) {
+	empStore.set(v)
+}
+
+export function getEmpProfile() {
+	return empProfile.get()
+}
+
+export function setEmpProfile(v) {
+	empProfile.set(v)
+}
+
+//
+
 class LocalStorage {
-	ls = {}
+	cache = {}
 
 	constructor(keys) {
 		keys.forEach(key => {
+			const store = new LocalStorageData(key)
+
 			Object.defineProperty(this, key, {
-				get: function() {
-					if (!this.ls[key]) {
-						this.ls[key] = localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : null
-					}
-					return this.ls[key]
+				get() {
+					const v = this.cache[key]
+					if (v) return v //^
+					return this.cache[key] = store.get()
 				},
 
-				set: function (json) {
-					this.ls[key] = json
-					const parsed = JSON.stringify(json)
-					localStorage.setItem(key, parsed)
-				}
+				set(value) {
+					this.cache[key] = store.set(value)
+				},
 			})
 		})
 	}
 }
 
-const myLocalStorage = new LocalStorage(["empStore", "empProfile"])
-
-export {myLocalStorage}
+export const myLocalStorage = new LocalStorage([ "empStore", "empProfile" ])
