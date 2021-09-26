41abd1701132c90102ba491f9860fb890d162584
Nemcron <goryashin.sanya@gmail.com>
2021-09-26 13:55:24 +0300

    PageIconForm: radio button's circles are now colored

--- src/pages/PageIconForm.vue
+++ src/pages/PageIconForm.vue
@@ -29,13 +29,13 @@
 							<v-col
 								cols="4"
 								v-for="color in iconColorsArr"
-								:key="color"
+								:key="color[0]"
 							>
 								<v-radio
 									class="d-inline-flex"
-									:label="color"
-									:color="color"
-									:value="color"
+									:label="color[0]"
+									:color="color[0]"
+									:value="color[0]"
 								/>
 							</v-col>
 						</v-row>
@@ -60,13 +60,13 @@ export default {
 
 	data: () => ({
 		iconColorsArr: [
-			"red", "pink", "purple", "deep-purple",
-			"indigo", "blue", "light-blue", "cyan",
-			"teal", "green", "light-green", "lime",
-			"yellow", "amber", "orange", "deep-orange",
-			"brown", "blue-grey", "grey", "black",
-			"white",
-		].sort(),
+			["red", "#F44336"], ["pink", "#E91E63"], ["purple", "#9C27B0"], ["deep-purple", "#673AB7"],
+			["indigo", "#3F51B5"], ["blue", "#2196F3"], ["light-blue", "#03A9F4"], ["cyan", "#00BCD4"],
+			["teal", "#009688"], ["green", "#4CAF50"], ["light-green", "#8BC34A"], ["lime", "#CDDC39"],
+			["yellow", "#FFEB3B"], ["amber", "#FFC107"], ["orange", "#FF9800"], ["deep-orange", "#FF5722"],
+			["brown", "#795548"], ["blue-grey", "#607D8B"], ["grey", "#9E9E9E"], ["black", "#000000"],
+			["white", "#FFFFFF"],
+		].sort( (prev, next) => prev[0] > next[0]),
 
 		values: {
 			icon: "",
@@ -85,7 +85,16 @@ export default {
 		],
 	}),
 
+	created() {
+	},
+
 	async mounted() {
+		// =======================================================================
+
+		document.querySelectorAll(".v-radio .v-icon")
+			.forEach((v, ind)=>v.style.color=this.iconColorsArr[ind][1])
+
+		// =======================================================================
 		this.values = _.clone(this.iconConfig)
 		await iconStorage.init()
 	},
