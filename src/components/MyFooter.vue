b794f155dda194e732711f23ebe3c9df8a059426
Alex <alex@yavent.ru>
2021-10-08 11:08:05 +0300

    feat(core): MyFooter: Composition & Option API mix sample - changeable year

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -4,17 +4,67 @@
 			class="text-center"
 			cols="12"
 		>
-			2021 — <strong>CheckPassport</strong>
+			<span class="pointer-cursor" @click="incYear">{{ year }}</span> —
+			<strong :class="['pointer-cursor', titleClass]" @click="decYear">CheckPassport</strong>
 		</v-col>
 	</v-footer>
 </template>
 
 <script>
+import { ref, computed } from "@vue/composition-api"
+
+function useYear(cur) {
+	const year = ref(cur)
+	return {
+		year,
+		isFuture: computed(() => year.value > cur + 1),
+		isPast: computed(() => year.value < cur - 1),
+		incYear: () => year.value += 1,
+	}
+}
+
+
 export default {
 	name: "MyFooter",
+
+	setup() {
+		// don't use "this" in setup()
+		return {
+			...useYear(2021),
+		}
+	},
+
+	// Composition & Option API mix sample
+
+	computed: {
+		titleClass() {
+			return this.isFuture
+				? "green--text"
+				: (this.isPast ? "grey--text text--darken-2" : undefined)
+		},
+	},
+
+	methods: {
+		decYear() {
+			this.year -= 1
+		},
+	},
 }
 </script>
 
-<style scoped>
+<style lang="sass">
+.no-select
+	user-select: none
+	// iOS Safari
+	-webkit-touch-callout: none
+	// Safari
+	-webkit-user-select: none
+	// Konqueror HTML
+	-khtml-user-select: none
+	// Old versions of Firefox
+	-moz-user-select: none
 
+.pointer-cursor
+	@extend .no-select
+	cursor: pointer
 </style>
