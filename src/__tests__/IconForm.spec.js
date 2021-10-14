91bea251467fbf545bca1250ae0adae683a6bef9
Nemcron <goryashin.sanya@gmail.com>
2021-10-14 20:07:46 +0300

    refactor(app): IconForm.spec.js: new test "prints an error on form submit if the icon's name input is empty" | IconForm: commented out scrolling to error

--- src/__tests__/IconForm.spec.js
+++ src/__tests__/IconForm.spec.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import Vuetify from "vuetify"
 import { createLocalVue, mount } from "@vue/test-utils"
+import flushPromises from 'flush-promises'
 import * as api from "@/utils/api"
 import IconForm from "@/components/IconForm"
 
@@ -8,34 +9,48 @@ const localVue = createLocalVue()
 Vue.use(Vuetify)
 
 describe("IconForm.vue", () => {
-	let wrapper
-	beforeEach(() => {
-		//FIXME поискать другой способ мокать функцию
-		api.getMdiIcons = jest.fn(() =>
-			Promise.resolve(["passport", "alert", "dog"])
-		)
-		const vuetify = new Vuetify()
+	//FIXME поискать другой способ мокать функцию
+	api.getMdiIcons = jest.fn(() =>
+		Promise.resolve(["passport", "alert", "dog"])
+	)
+
+	const vuetify = new Vuetify()
+
+	const createWrapper = iconData => mount(IconForm, {
+		localVue,
+		vuetify,
+		propsData: { iconData }
+	})
+
+	it("renders chip for each icon in props.iconData.icon", async () => {
 		const iconData = {
-			icon: "alert",
-			size: "400",
+			icon: ["passport", "alert"],
+			size: "60",
 			color: "white",
 		}
+		const wrapper = createWrapper(iconData)
 
-		wrapper = mount(IconForm, {
-			localVue,
-			vuetify,
-			propsData: { iconData }
-		})
-	})
-
-	afterEach(() => {
-		wrapper.destroy()
-	})
-
-
-	it("renders chip for each icon in props.iconData.icon", () => {
+		await flushPromises()
 		const autocomplete = wrapper.find(".v-autocomplete")
-		const chipLen = autocomplete.findAll(".v-chip").length
-		expect(chipLen).toBe(1)
+		const chips = autocomplete.findAll(".v-chip")
+
+		expect(chips.length).toBe(iconData.icon.length)
+	})
+
+	it("prints an error on form submit if the icon's name input is empty", async () => {
+		const iconData = {
+			icon: [],
+			size: "60",
+			color: "white",
+		}
+		const wrapper = createWrapper(iconData)
+
+		await flushPromises()
+		const form =  wrapper.find(".v-form")
+		form.trigger('submit.prevent')
+
+		await flushPromises()
+		const error = wrapper.find(".v-messages__message")
+		expect(error.exists()).toBe(true)
 	})
 })
--- src/components/IconForm.vue
+++ src/components/IconForm.vue
@@ -179,14 +179,14 @@ export default {
 
 		submit() {
 			const isValid = this.$refs.theForm.validate()
-			if (!isValid) {
-				this.$nextTick(() => {
-					const errorEl = document.querySelector(".v-messages__message:first-of-type")
-					const inputEl = errorEl.parentNode.parentNode.parentNode.parentNode
-					this.$vuetify.goTo(inputEl)
-				})
-				return //^
-			}
+			// if (!isValid) {
+			// 	this.$nextTick(() => {
+			// 		const errorEl = document.querySelector(".v-messages__message:first-of-type")
+			// 		const inputEl = errorEl.parentNode.parentNode.parentNode.parentNode
+			// 		this.$vuetify.goTo(inputEl)
+			// 	})
+			// 	return //^
+			// }
 			this.$emit("save", this.values)
 		},
 
