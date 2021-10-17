1f81c6b19e0bdf7060cee080fd85a25e8ca479d3
Nemcron <goryashin.sanya@gmail.com>
2021-10-17 18:16:51 +0300

    refactor(app): IconForm.spec.js: optimized tests | fixed errors | added new test "emits 'save' when form is submitted"

--- src/__tests__/IconForm.spec.js
+++ src/__tests__/IconForm.spec.js
@@ -5,57 +5,75 @@ import flushPromises from 'flush-promises'
 import * as api from "@/utils/api"
 import IconForm from "@/components/IconForm"
 
-const localVue = createLocalVue()
 // localVue.use(Vuetify) - так нельзя
 Vue.use(Vuetify)
 
-describe("IconForm.vue", () => {
-	test("sanity test", () => {
-		console.log(IconForm)
+describe("IconForm.vue: multiple=true", () => {
+	let wrapper
+	const iconData = {
+		icon: ["passport", "alert"],
+		size: "60",
+		color: "white",
+	}
+
+	beforeEach(async () => {
+		const localVue = createLocalVue()
+		const vuetify = new Vuetify()
+
+		let node = document.createElement("div")
+		node.setAttribute("id", "app")
+		node.setAttribute("data-app", "true")
+		console.log("outer:\n", node.outerHTML) //D
+		document.body.appendChild(node)
+
+		wrapper = mount(IconForm, {
+			localVue,
+			vuetify,
+			propsData: { iconData },
+			attachTo: "#app"
+		})
+		await flushPromises()
 	})
-	//FIXME поискать другой способ мокать функцию
+
+	afterEach(() => {
+		wrapper.destroy()
+	})
+
 	api.getMdiIcons = jest.fn(() =>
 		Promise.resolve(["passport", "alert", "dog"])
 	)
 
-	const vuetify = new Vuetify()
-
-	const createWrapper = iconData => mount(IconForm, {
-		localVue,
-		vuetify,
-		propsData: { iconData },
-		attachToDocument: true
+	test("sanity test", () => {
+		console.log(IconForm)
 	})
 
 	it("renders chip for each icon in props.iconData.icon", async () => {
-		const iconData = {
-			icon: ["passport", "alert"],
-			size: "60",
-			color: "white",
-		}
-		const wrapper = createWrapper(iconData)
-
-		await flushPromises()
 		const autocomplete = wrapper.find(".v-autocomplete")
 		const chips = autocomplete.findAll(".v-chip")
 
-		expect(chips.length).toBe(iconData.icon.length)
+		expect(chips).toHaveLength(iconData.icon.length)
 	})
 
-	it("prints an error on form submit if the icon's name input is empty", async () => {
+	it("renders an error message on form submit if the icon's name input is empty", async () => {
 		const iconData = {
 			icon: [],
 			size: "60",
 			color: "white",
 		}
-		const wrapper = createWrapper(iconData)
+		wrapper.setProps({iconData})
 
+		const form =  wrapper.find(".v-form")
+		form.trigger('submit.prevent')
 		await flushPromises()
+		const error = wrapper.find(".v-messages__message")
+
+		expect(error.exists()).toBe(true)
+	})
+
+	it("emits 'save' when form is submitted", async () => {
 		const form =  wrapper.find(".v-form")
 		form.trigger('submit.prevent')
 
-		await flushPromises()
-		const error = wrapper.find(".v-messages__message")
-		expect(error.exists()).toBe(true)
+		expect(wrapper.emitted("save")).toHaveLength(1)
 	})
 })
